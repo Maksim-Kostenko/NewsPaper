e69051b34fad5f25421ffebe2b321ee067fa6c24
Maksim-Kostenko <kostenckomaks1@gmail.com>
2025-06-29 01:29:06 +0400

    Созданы задания, а также requirements

--- news/views.py
+++ news/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from news.models import Post
 
 
 # Create your views here.
+#todo: 1. Скорректировать view и html, чтобы было более читабельно, а также скорректировать html
+#todo: 2. Выводите новости в следующем виде — заголовок, дата публикации в формате день.месяц.год, затем первые 20 слов текста статьи. Можно вывести как списком, так и таблицей. Новости должны выводиться в порядке от более свежей к старой.
+#todo: 3. Сверху страницы должно быть выведено количество всех новостей (используется фильтр news|length).
+#todo: 4. По ссылке /news/<id новости> должна выводиться детальная информация о новости.
+#todo: 5. Заголовок, дата публикации в формате день.месяц.год и полный текст статьи.
 
 class NewsList(ListView):
     model = Post
@@ -16,3 +20,4 @@ class NewsDetail(DetailView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'post'
+
